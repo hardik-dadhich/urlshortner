1381022c73bfd87713c446428242a536397dc17f
Hardik dadhich <dadhichhardik26@gmail.com>
2021-03-14 01:56:36 +0530

    Improvise Logic of shortining

--- api/models.py
+++ api/models.py
@@ -1,32 +1,30 @@
 from django.db import models
 import os
 import random
-
+import string
 # Create your models here.
 counter = 0
+
+list_of_objects_short = dict()
+list_of_objects_long = dict()
 
 class UrlModel(object):
 
     '''
     this class will store the url's mapping and works as db
     '''
-
-    list_of_objects = {}
-
-    def __init__(self, long_url):
-        self._id = counter+1
-        self.long_url = long_url
-        self.short_url = None
+    global list_of_objects_short
 
     def mapper(self, long_url, short_url):
-        self.list_of_objects[long_url] = short_url
+        list_of_objects_short[long_url] = short_url
+        list_of_objects_long[short_url] = long_url
 
-    
-    def retrun_short_url(self):
-        return self.short_url
+    def retrun_short_url(self, url):
+        return list_of_objects_short[url]
 
-    def return_long_url(self):
-        return self.long_url
+    def __str__(self):
+        return list_of_objects_short[url]
+
 
 def create_random_encode():
 
@@ -34,15 +32,27 @@ def create_random_encode():
     function returns three random char for context route
     '''
 
-    choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    choice_of_caps = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'Q', 'R', 'S', 'T']
-    context = ''
-    context = random.choice(choices)
-    context+=random.choice(choice_of_caps)
-    return context
+    # choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    # choice_of_caps = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'Q', 'R', 'S', 'T']
+    # context = ''
+    # context = random.choice(choices)
+    # context+=random.choice(choice_of_caps)
+    # return context
 
+    # improved logic
+    letters = string.ascii_lowercase + string.ascii_uppercase
+    while True:
+        rand_letters = random.choices(letters, k=3)
+        rand_letters = "".join(rand_letters)
+        if rand_letters not in list_of_objects_short.keys():
+            return rand_letters
+
+
+def create_short_url(url):
+    #print("fucntion call")
+    main_url = 'http://127.0.0.1:8000/'
+    short_url = main_url+create_random_encode()
+    obj = UrlModel()
+    obj.mapper(url, short_url)
+    return list_of_objects_short[url]
     
-
-def main_method():
-    db = UrlModel(long_url)
-
--- api/views.py
+++ api/views.py
@@ -1,17 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound
 from api import models as db_model
 # Create your views here.
+ans = ''
+
 def home(request):
     if request.method == "POST":
-        url_recieved = request.form["incoming_url"]
-        url_found = db_model.UrlModel.return_long_url
-        #print(url_recieved)
-        return url_recieved 
+        url_recieved = request.POST.get("incoming_url")
+        
+        if url_recieved not in db_model.list_of_objects_short.keys():
+            ans = db_model.create_short_url(url_recieved)
+            return render(request, "result.html", {'ans' : ans})
+        else:
+            # the url already present in the DB so return present one
+            ans = db_model.list_of_objects_short[url_recieved]
+            #print("ans is " , ans)
+            
+            return render(request, "result.html", {'ans' : ans})
+
     return render(request, "index.html", {})
 
 def result(request):
     if request.method == "POST":
+        url_recieved = request.POST.get("incoming_url")
+        
+        if url_recieved not in db_model.list_of_objects_short.keys():
+            ans = db_model.create_short_url(url_recieved)
+            #print("-------------------->", ans)
+            origional_link = db_model.list_of_objects_long[ans]
+            return render(request, "result.html", {'ans' : ans, 'origional_link' : origional_link })
+        else:
+            # the url already present in the DB so return present one
+            ans = db_model.list_of_objects_short[url_recieved]
+            #origional_link = ans.split('/')[1]
+            origional_link = db_model.list_of_objects_long[ans]
+            return render(request, "result.html", {'ans' : ans, 'origional_link' : origional_link})
         return render(request, "result.html", {})
     else:
         return HttpResponseNotFound('<h1>Page not found</h1>')
+
+
+def redirection_function(short_url):
+    long_url = db_model.list_of_objects_long[short_url]
+    if long_url:
+        return redirect(long_url)
+    return HttpResponseNotFound('<h1>Page not found</h1>')
