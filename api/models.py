42adcd279c074d91ab0b03a158c369ff05eb667a
Hardik dadhich <dadhichhardik26@gmail.com>
2021-03-13 15:14:46 +0530

    Creating DB mapping

--- api/models.py
+++ api/models.py
@@ -1,46 +1,48 @@
 from django.db import models
-#from tinydb import TinyDB
 import os
 import random
 
 # Create your models here.
+counter = 0
 
-class Userinput():
+class UrlModel(object):
 
     '''
-    this class will take user input
+    this class will store the url's mapping and works as db
     '''
 
-    def create_random(self):
-        '''
-        function returns thee random context
-        '''
-        
-        choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-        choice_of_caps = ['A', 'B', 'C', 'D', 'E' ]
-        context = ''
-        context = random.choice(choices)
-        context+=random.choice(choice_of_caps)
-        return context
+    list_of_objects = {}
 
-    def tackle_input(self, url):
+    def __init__(self, long_url):
+        self._id = counter+1
+        self.long_url = long_url
+        self.short_url = None
 
-        data_base = {}
-        if url not in data_base:
-            pass
+    def mapper(self, long_url, short_url):
+        self.list_of_objects[long_url] = short_url
 
-    # def create_db(userinput):
-    #     dir_path = os.getcwd()
-    #     print(dir_path)
-    #     list_files = os.listdir(dir_path) 
-    #     if 'data.txt' not in list_files: 
-    #         with open(os.path.join(dir_path/, 'data.txt'), 'w') as fp:
-    #             fp.write(userinput, 'a+')
-
-
-
-   
-
-u = Userinput()
-print(u.create_random())
     
+    def retrun_short_url(self):
+        return self.short_url
+
+    def return_long_url(self):
+        return self.long_url
+
+def create_random_encode():
+
+    '''
+    function returns three random char for context route
+    '''
+
+    choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    choice_of_caps = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'Q', 'R', 'S', 'T']
+    context = ''
+    context = random.choice(choices)
+    context+=random.choice(choice_of_caps)
+    return context
+
+    
+
+def main_method():
+    db = UrlModel(long_url)
+
--- api/views.py
+++ api/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
-
+from api import models as db_model
 # Create your views here.
 def home(request):
+    if request.method == "POST":
+        url_recieved = request.form["incoming_url"]
+        url_found = db_model.UrlModel.return_long_url
+        #print(url_recieved)
+        return url_recieved 
     return render(request, "index.html", {})
 
 def result(request):
